4bc82f16f34acdd4668735277f5e45baa30151a0
Yousaf Nabi <yousaf@pactflow.io>
2025-01-24 17:03:34 +0000

    chore: fmt fix

--- examples/graphql/src/consumer.spec.ts
+++ examples/graphql/src/consumer.spec.ts
@@ -3,12 +3,7 @@ import * as chai from 'chai';
 import * as path from 'path';
 import chaiAsPromised from 'chai-as-promised';
 import { query } from './consumer';
-import {
-  Pact,
-  GraphQLInteraction,
-  Matchers,
-  LogLevel,
-} from '@you54f/pact';
+import { Pact, GraphQLInteraction, Matchers, LogLevel } from '@you54f/pact';
 const { like } = Matchers;
 const LOG_LEVEL = process.env.LOG_LEVEL || 'TRACE';
 
